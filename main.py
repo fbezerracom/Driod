c37c8229e5884058667d702f1c7bb12ee2e3c904
fbezerra.com <fbezerra.com@gmail.com>
2021-08-30 22:09:54 -0300

    IA

---
+++ main.py
@@ -0,0 +1,12 @@
+# Our main file.
+
+import speech_recognition as sr
+
+# Cria um reconhecedor
+r = sr.Recognizer()
+
+# Abrir o microfone para captura
+with sr.Microphone() as source:
+    audio = r.listen(source) # Define o microfone como fonte de áudio
+
+    print(r.recognize_google(audio))
