3b1e4afd3a4dc242273c49d464a14b26417ea568
fbezerra.com <fbezerra.com@gmail.com>
2021-08-31 20:43:54 -0300

    Prestar atenção no buffer do pyaudio
    Buffer 2048 parece ok.

---
+++ core/__init__.py
@@ -0,0 +1,13 @@
+import datetime
+
+# 2015 5 6 8 53 40
+
+class SystemInfo:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def get_time():
+        now = datetime.datetime.now()
+        answer = 'São {} horas e {} minutos.'.format(now.hour, now.minute)
+        return answer
