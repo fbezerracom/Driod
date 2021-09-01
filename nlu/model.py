14e2746afc032343f4ef9152c384a1d724e97da7
fbezerra.com <fbezerra.com@gmail.com>
2021-08-31 22:34:59 -0300

    Adicionar modelo
    Modelo de classificação de texto para NLU.

---
+++ nlu/model.py
@@ -0,0 +1,15 @@
+import yaml
+
+
+data = yaml.safe_load(open('nlu\\train.yml', 'r', encoding='utf-8').read())
+
+inputs, outputs = [], []
+
+for command in data['commands']:
+    inputs.append(command['input'])
+    outputs.append('{}\{}'.format(command['entity'], command['action']))
+
+'''
+print(inputs)
+print(outputs)
+'''
